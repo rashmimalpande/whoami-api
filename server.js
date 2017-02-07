0a9872d3b5730b3771154ae010eaedfa7abd406e
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-07 09:15:40 +0000

    Add user agent to the response

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.enable("trust proxy");
 
 app.get('/api/whoami', function (req, res) {
-  res.json({"ipaddress": req.ip, "language": req.acceptsLanguages()});
+  res.json({"ipaddress": req.ip, "language": req.acceptsLanguages(), "software": req.headers["user-agent"]});
 });
 
 app.listen(process.env.PORT || 8080);
